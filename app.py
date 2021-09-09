0c6d09a960f3531519693be3f61bebaa286c663b
Peng Li <justdoit.pli@gmail.com>
2021-09-09 15:53:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import json
 import requests
 
 option_a = os.getenv('OPTION_A', u"Horses 🐺")
-option_b = os.getenv('OPTION_B', u"bbbb 🐶")
+option_b = os.getenv('OPTION_B', u"cccc 🐶")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
