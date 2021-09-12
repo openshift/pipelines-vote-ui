99a17c5b212226686486c09540ec55c2a1f17322
Peng Li <justdoit.pli@gmail.com>
2021-09-12 08:51:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import random
 import json
 import requests
 
-option_a = os.getenv('OPTION_A', u"Horse 🐺")
-option_b = os.getenv('OPTION_B', u"Dog 🐶")
+option_a = os.getenv('OPTION_A', u"Horses 🐺")
+option_b = os.getenv('OPTION_B', u"Dogs 🐶")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
