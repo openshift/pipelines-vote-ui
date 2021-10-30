21750d9f9dcd65a5d7ecb72c768154ea1b19dfb9
miles-canton <54526353+miles-canton@users.noreply.github.com>
2021-10-30 14:13:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import random
 import json
 import requests
 
-option_a = os.getenv('OPTION_A', u"Horses 🐺")
-option_b = os.getenv('OPTION_B', u"DDDDHOT 🐶")
+option_a = os.getenv('OPTION_A', u"Horses-1030 🐺")
+option_b = os.getenv('OPTION_B', u"DDDDHOT-1030 🐶")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
